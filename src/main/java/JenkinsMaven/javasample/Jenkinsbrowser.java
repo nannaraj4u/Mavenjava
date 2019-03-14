19447f1f504cf8bb553542cb39549b37d1313248
rajashekar <nannarajashekar@gmail.com>
2019-03-14 11:02:50 +0530

    first commit

---
+++ src/main/java/JenkinsMaven/javasample/Jenkinsbrowser.java
@@ -0,0 +1,27 @@
+package JenkinsMaven.javasample;
+
+
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Jenkinsbrowser {
+
+public static void main(String[] args) {
+
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\rajashekar.n\\Desktop\\Drivers\\chromedriver.exe");
+
+// Initialize browser
+WebDriver driver=new ChromeDriver();
+
+// Open facebook
+driver.get("http://www.facebook.com");
+
+// Maximize browser
+
+driver.manage().window().maximize();
+
+}
+
+}
